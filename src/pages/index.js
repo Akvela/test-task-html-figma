a85b0b41ae16c381efd24e2c613e0486d4d12224
Akvela <akvela25@yandex.ru>
2023-02-16 00:35:48 +0300

    добавлен адаптив на все брейкпоинты

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,38 @@
 import './index.css';
+
+const inputCarCost = document.getElementById('car-cost');
+const inputRangeCarCost = document.getElementById('car-cost-range');
+const inputAnInitialFee = document.getElementById('an-initial-fee');
+const inputRangeAnInitialFee = document.getElementById('an-initial-fee-range');
+const inputRentalPeriod = document.getElementById('rental-period');
+const inputRangeRentalPeriod = document.getElementById('rental-period-range');
+const percent = document.getElementById('percent');
+
+inputRangeCarCost.addEventListener('input', () => {
+  const value = inputRangeCarCost.value
+  inputRangeCarCost.style.background = `-webkit-linear-gradient(left, #FF9514 0%, #FF9514 ${value}%, #E1E1E1 ${value}%, #E1E1E1 100%)`
+  
+  if (inputRangeCarCost.value > 14) {
+    inputCarCost.value = inputRangeCarCost.value * 100000;
+  }
+});
+
+inputRangeAnInitialFee.addEventListener('input', () => {
+  const value = (inputRangeAnInitialFee.value - 10) * 2;
+  inputRangeAnInitialFee.style.background = `-webkit-linear-gradient(left, #FF9514 0%, #FF9514 ${value}%, #E1E1E1 ${value}%, #E1E1E1 100%)`
+  
+  if (inputRangeAnInitialFee.value > 9 && inputRangeAnInitialFee.value < 61) {
+    inputAnInitialFee.value = inputCarCost.value / 100 * inputRangeAnInitialFee.value;
+    percent.textContent = inputRangeAnInitialFee.value + '%';
+    
+  }
+});
+
+inputRangeRentalPeriod.addEventListener('input', () => {
+  const value = inputRangeRentalPeriod.value * 100 / 120;
+  inputRangeRentalPeriod.style.background = `-webkit-linear-gradient(left, #FF9514 0%, #FF9514 ${value}%, #E1E1E1 ${value}%, #E1E1E1 100%)`
+
+  if (inputRangeRentalPeriod.value > 5 && inputRangeRentalPeriod.value < 121) {
+    inputRentalPeriod.value = parseInt(inputRangeRentalPeriod.value);
+  }
+});
